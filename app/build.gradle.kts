32325f9972e888c95bb8ee6511f3d18e8257cd7e
Paolo <p67906265-spec@users.noreply.github.com>
2026-08-18 09:44:03 +0200

    Versione 0.21 modifiche pannello PIN

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.paolo.passwordsafe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.20"
+        versionCode = 21
+        versionName = "0.21"
     }
 
     signingConfigs {
