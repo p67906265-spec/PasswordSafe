a373bebac954cbb61dc112a0fd9e0245f23830d9
Paolo <p67906265-spec@users.noreply.github.com>
2026-08-22 16:14:14 +0200

    PasswordSafe 0.54 aggiornamento Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.paolo.passwordsafe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 53
-        versionName = "0.53"
+        versionCode = 54
+        versionName = "0.54"
     }
 
     signingConfigs {
